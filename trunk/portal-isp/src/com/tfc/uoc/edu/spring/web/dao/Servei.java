dbb8ed38138ef9ae89bd771f9fd0450dfa43ebbd
eduardgubert@gmail.com <eduardgubert@gmail.com@b2fe15ed-3a92-1e64-847c-8827442be017>
2015-01-11 02:45:29 +0000

    Codificació inicial de les classes: Servei, ServeiDomini i ServeiAllotjament

--- trunk/portal-isp/src/com/tfc/uoc/edu/spring/web/dao/Servei.java
+++ trunk/portal-isp/src/com/tfc/uoc/edu/spring/web/dao/Servei.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 @Entity
 @Table(name="serveis")
-public class Servei {
+public abstract class Servei {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ trunk/portal-isp/src/com/tfc/uoc/edu/spring/web/dao/ServeiAllotjament.java
@@ -0,0 +1,11 @@
+package com.tfc.uoc.edu.spring.web.dao;
+
+import java.util.List;
+
+public class ServeiAllotjament {
+		
+	private int idServidor; // Servidor on està ubicat l'allotjament
+	
+	
+
+}
