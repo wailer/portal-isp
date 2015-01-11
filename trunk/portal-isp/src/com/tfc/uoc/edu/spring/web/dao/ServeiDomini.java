724bb4c86cbab5195c265dbea4b67ddd135102a2
eduardgubert@gmail.com <eduardgubert@gmail.com@b2fe15ed-3a92-1e64-847c-8827442be017>
2015-01-11 14:59:30 +0000

    Implementar jerarquia de serveis , persistència i tests bàsics

--- trunk/portal-isp/src/com/tfc/uoc/edu/spring/web/dao/ServeiDomini.java
+++ trunk/portal-isp/src/com/tfc/uoc/edu/spring/web/dao/ServeiDomini.java
@@ -1,10 +1,48 @@
 package com.tfc.uoc.edu.spring.web.dao;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
-public class ServeiDomini {
+@Entity
+@Table(name = "serveis")
+@DiscriminatorValue("Domini")
+public class ServeiDomini extends Servei {
 	
+
+
 	@NotNull
 	private String nom;    //nom del domini sense el tld
+	
+	public ServeiDomini(String nom,Domini domini,User user) {
+		super(domini,user);
+		this.setEstat("CISTELLA");
+		this.setProducte(domini);
+		this.nom = nom;
+	}
+	
+	public ServeiDomini() {
+		
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	@Override
+	public String toString() {
+		return "ServeiDomini [nom=" + nom + ", getNom()=" + getNom()
+				+ ", hashCode()=" + hashCode() + ", getClass()=" + getClass()
+				+ ", toString()=" + super.toString() + "]";
+	}
+
+
+	
 
 }
--- trunk/portal-isp/src/com/tfc/uoc/edu/spring/web/dao/User.java
+++ trunk/portal-isp/src/com/tfc/uoc/edu/spring/web/dao/User.java
@@ -1,5 +1,6 @@
 package com.tfc.uoc.edu.spring.web.dao;
 
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -86,7 +87,7 @@ public class User {
 	private String poblacio;
 
 	private String empresa;
-
+	
 	public User() {
 
 	}
